5692037d95dfcf6faf92f518959262d299273b65
Robert Pito Sanchez <robsanchez124@gmail.com>
2024-09-07 13:52:19 -0400

    adding menu button

--- main.js
+++ main.js
@@ -1,2 +1,22 @@
-import "./node_modules/bootstrap/dist/css/bootstrap.min.css"
-import { Tooltip } from "bootstrap"
+import "./style.css"
+
+function toggleMenu() {
+    const menu = document.getElementById('menuPage');
+    if (menu) {
+        menu.style.display = menu.style.display === 'block' ? 'none' : 'block';
+    }
+}
+
+
+// JavaScript for dynamic words
+const words = ['PRODUCER', 'NOVELIST', 'PLAYWRIGHT', 'DIRECTOR'];
+let currentIndex = 0;
+const dynamicWordsElement = document.getElementById('dynamic-words');
+
+function updateWord() {
+    dynamicWordsElement.innerHTML = `<span>${words[currentIndex]}</span>`;
+    currentIndex = (currentIndex + 1) % words.length;
+}
+
+setInterval(updateWord, 2000);
+updateWord(); // Initial call to set the first word
